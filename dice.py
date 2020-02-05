88d7ed3b371239d7b2d1cde4da290908338b79f9
breakthatbass <gamache.taylor@gmail.com>
2020-02-04 21:34:57 -0500

    add modifier component

--- dice.py
+++ dice.py
@@ -1,22 +1,40 @@
 import sys
 from random import randint
+from termcolor import colored
 
 def main():
-    if len(sys.argv) != 3:
-        print("Usage: python dice.py dice-type rolls")
+    if len(sys.argv) < 3:
+        print("Usage: python dice.py dice-type rolls modifier")
+        return 1
     
     die = int(sys.argv[1])
     rolls = int(sys.argv[2])
+    modifier = 0
 
-    for i in range(rolls):
-        cast = randint(1, die)
+    if len(sys.argv) == 4:
+        modifier = sys.argv[3]
+    
+    def roll_dice(die, rolls, modifier):
+        # var to deal with the modifier complexity
+        if modifier == '':
+            modifier = int(0)
+        print('----------------')
+        for i in range(rolls):
+            cast = randint(1, die)
+            
+            if modifier != 0:
+                print(f'base cast is {cast}')
+                print(colored(f"modifier is {modifier}", 'blue'))
+            
+            if cast == 20:
+                print(colored(f"Roll {i + 1} is 20! Critical Hit!", 'green'))
+            elif cast == 1 and die == 20:
+                print(colored(f"Roll {i + 1} is 1. Critical miss! Dangit!", 'red'))
+            else:
+                print(f"Roll {i + 1} is {cast + (int(modifier))}")
+            print('----------------')
 
-        if cast == 20:
-            print("Roll {} is 20! Critical Hit!".format(i + 1))
-        elif cast == 1 and die == 20:
-            print("Roll {} is 1. Critical miss! Dangit!".format(i + 1))
-        else:
-            print("Roll {} is {}".format(i + 1, cast))
+    roll_dice(die, rolls, modifier)
 
 if __name__ == "__main__":
     main()
